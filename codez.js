298b32da226a345a123b173269ad39a725049e6a
= <klemmer@uni-bremen.de>
2019-11-23 00:33:26 +0100

    software

--- codez.js
+++ codez.js
@@ -19,12 +19,7 @@ $( document ).ready(function() {
         $("#tab-guests").hide();
         $("#tab-homepage").hide();
         
-        var rules = erratic.parse(spec);
-        
-        $("#spec-cpu").html(erratic.generate(rules, 'cpu'));
-        $("#spec-ram").html(erratic.generate(rules, 'ram'));
-        $("#spec-mousepad").html(erratic.generate(rules, 'mousepad'));
-        $("#spec-application1").html(erratic.generate(rules, 'application1'));
+        generate();
     })
 
     $("#btn-guests").click(function() {
@@ -47,9 +42,29 @@ $( document ).ready(function() {
         $("#tab-guests").hide();
         $("#tab-homepage").hide();
         
-        var rules = erratic.parse(spec);
-
-        $("#spec-cpu").html(erratic.generate(rules, 'cpu'));
-        $("#spec-ram").html(erratic.generate(rules, 'ram'));
-    })
+        generate();
+    });
+    generate();
 });
+
+function generate() {
+    var rules = erratic.parse(spec);
+        
+    $("#spec-cpu").html(erratic.generate(rules, 'cpu'));
+    $("#spec-ram").html(erratic.generate(rules, 'ram'));
+    $("#spec-mousepad").html(erratic.generate(rules, 'mousepad'));
+    $("#spec-application1").html(erratic.generate(rules, 'application1'));
+
+    $("#software1").html(erratic.generate(rules, 'software'));
+    $("#software1-version").html(erratic.generate(rules, 'version'));
+    $("#software2").html(erratic.generate(rules, 'software'));
+    $("#software2-version").html(erratic.generate(rules, 'version'));
+    $("#software3").html(erratic.generate(rules, 'software'));
+    $("#software3-version").html(erratic.generate(rules, 'version'));
+    $("#software4").html(erratic.generate(rules, 'software'));
+    $("#software4-version").html(erratic.generate(rules, 'version'));
+    $("#software5").html(erratic.generate(rules, 'software'));
+    $("#software5-version").html(erratic.generate(rules, 'version'));
+    $("#software6").html(erratic.generate(rules, 'software'));
+    $("#software6-version").html(erratic.generate(rules, 'version'));
+}
