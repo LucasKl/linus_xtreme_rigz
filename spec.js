cde3f7a69bb7c505881dea97a0974b81306259f2
Nikolas <nikmaehl@uni-bremen.de>
2019-11-22 23:39:23 +0100

    a

--- spec.js
+++ spec.js
@@ -1,4 +1,5 @@
-let spec=`<ram> ::= ""<ramManufacturer>" "<ramNumber>" "<ramUnit>" "<ramType>"" | ""<ramManufacturer>"" | "Kingston" | "Razer" | "Corsair";
+let spec=`<ram> ::= ""<ramManufacturer>" "<ramNumber>" "<ramUnit>" "<ramType>"";
+<ramManufacturer> ::= "Kingston" | "Razer" | "Corsair";
 <ramNumber> ::= "2" | "4" | "8" | "10" | "12" | "1024" | "2048";
 <ramUnit> ::= "GB" | "MB" | "MiBi" | "GiBi" | "KB" | ""<ramType>"" | "DDR2" | "SRAM" | "DDR3" | "DDR3000000" | "Flash";
 <cpu> ::= "i3" | "i5" | "i7" | "Pentium I" | "Pentium II" | "MIPS 500";
