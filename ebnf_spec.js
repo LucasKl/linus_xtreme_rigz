63f24e73438a8719d8bf74b43cec54ecb0914d8e
Nikolas <nikmaehl@uni-bremen.de>
2019-11-22 23:35:45 +0100

    grammar

--- ebnf_spec.js
+++
@@ -1,5 +0,0 @@
-let spec=`<ram> ::= ""<number>" "<unit>"";
-<number> ::= "10" | "16" | "1024";
-<unit> ::= "GB" | "MB" | "MiBi";
-<cpu> ::= "i3" | "i5" | "i7";
-`
---
+++ spec.js
@@ -0,0 +1,8 @@
+let spec=`<ram> ::= ""<ramManufacturer>" "<ramNumber>" "<ramUnit>" "<ramType>"" | ""<ramManufacturer>"" | "Kingston" | "Razer" | "Corsair";
+<ramNumber> ::= "2" | "4" | "8" | "10" | "12" | "1024" | "2048";
+<ramUnit> ::= "GB" | "MB" | "MiBi" | "GiBi" | "KB" | ""<ramType>"" | "DDR2" | "SRAM" | "DDR3" | "DDR3000000" | "Flash";
+<cpu> ::= "i3" | "i5" | "i7" | "Pentium I" | "Pentium II" | "MIPS 500";
+<mousepad> ::= "League of Legends" | "Naruto" | "YuGiOh";
+<mouse> ::= "Trackball" | "Ergo Plus" | "Logitech G203" | "Sharkoon Drakonia Black" | "Razer Deathadder Elite Pro" | "Redragon M908 Impact RGB";
+<tastature> ::= "Cherry";
+`
