1e9b7ab1aea195badb140ecb84900394573b7b0b
= <klemmer@uni-bremen.de>
2019-11-23 01:11:10 +0100

    toolbars

--- codez.js
+++ codez.js
@@ -58,9 +58,14 @@ function generate() {
     $("#spec-mouse").html(erratic.generate(rules, 'mouse'));
     $("#spec-keyboard").html(erratic.generate(rules, 'keyboard'));
     $("#spec-storage").html(erratic.generate(rules, 'storage'));
-    // $("#spec-display").html(erratic.generate(rules, 'display'));
+    $("#spec-display").html(erratic.generate(rules, 'display'));
     // $("#spec-printer").html(erratic.generate(rules, 'printer'));
     $("#spec-mousepad").html(erratic.generate(rules, 'mousepad'));
+
+    $("#spec-toolbar1").html(erratic.generate(rules, 'toolbar1'));
+    $("#spec-toolbar2").html(erratic.generate(rules, 'toolbar2'));
+    $("#spec-toolbar3").html(erratic.generate(rules, 'toolbar3'));
+    $("#spec-toolbar4").html(erratic.generate(rules, 'toolbar4'));
 
     $("#software1").html(erratic.generate(rules, 'software'));
     $("#software1-version").html(erratic.generate(rules, 'version'));
